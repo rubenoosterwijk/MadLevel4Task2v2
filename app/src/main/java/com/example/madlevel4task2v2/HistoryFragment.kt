608b83655a216c14a0521989c4c2608af8420c82
Ruben <ruben.oosterwijk@hva.nl>
2020-11-12 15:17:03 +0100

    Auto stash before rebase of "refs/heads/main"

--- app/src/main/java/com/example/madlevel4task2v2/HistoryFragment.kt
+++ app/src/main/java/com/example/madlevel4task2v2/HistoryFragment.kt
@@ -1,10 +1,8 @@
 package com.example.madlevel4task2v2
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
@@ -13,12 +11,25 @@ import androidx.navigation.fragment.findNavController
  */
 class HistoryFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enabling option menu
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        // Title of fragment
+        (activity as MainActivity).supportActionBar?.title = "Your Game History"
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
