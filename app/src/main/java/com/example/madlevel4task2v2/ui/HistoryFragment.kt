25370640d3700d2ef95b89633a853d536b1e91a8
Ruben <ruben.oosterwijk@hva.nl>
2020-11-13 01:03:13 +0100

    Historyscreen now shows played games with delete button functionality

--- app/src/main/java/com/example/madlevel4task2v2/ui/HistoryFragment.kt
+++ app/src/main/java/com/example/madlevel4task2v2/ui/HistoryFragment.kt
@@ -3,12 +3,28 @@ package com.example.madlevel4task2v2.ui
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel4task2v2.R
+import com.example.madlevel4task2v2.adapter.GameAdapter
+import com.example.madlevel4task2v2.model.Game
+import com.example.madlevel4task2v2.repository.GameRepository
+import kotlinx.android.synthetic.main.fragment_history.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class HistoryFragment : Fragment() {
+
+    private val gameList = arrayListOf<Game>()
+    private val gameAdapter = GameAdapter(gameList)
+    private var historyCleared = false
+    private lateinit var gameRepository: GameRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +46,11 @@ class HistoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
+        gameRepository = GameRepository(requireContext())
 
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_delete, menu)
@@ -41,10 +60,41 @@ class HistoryFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.delete -> {
-
+                deleteGameHistory()
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initViews() {
+        rv_games.apply {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+            adapter = gameAdapter
+        }
+        getGamesFromDatabase()
+        gameAdapter.notifyDataSetChanged()
+    }
+
+    private fun getGamesFromDatabase() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val gameList = withContext(Dispatchers.IO) {
+                gameRepository.getAllGames()
+            }
+            this@HistoryFragment.gameList.clear()
+            this@HistoryFragment.gameList.addAll(gameList)
+            this@HistoryFragment.gameAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun deleteGameHistory() {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.IO) {
+                gameRepository.deleteAllGames()
+            }
+            historyCleared = true
+            getGamesFromDatabase()
         }
     }
 }
